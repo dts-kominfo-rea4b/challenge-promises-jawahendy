2f93d578d35078a6e4616f3816e3c2064975f58a
jawahendy <hendyn25@gmail.com>
2022-09-16 11:16:17 +0700

    Hendy Nurfrianto

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  const filmIXX = await promiseTheaterIXX();
+  const filmVGC = await promiseTheaterVGC();
+  const Joinfilm = [...filmIXX, ...filmVGC];
+  const newarr = [];
+
+  return new Promise((resolve) => {
+    for (film of Joinfilm) {
+      if (film.hasil == hasil) {
+        newarr.push(film);
+      }
+    }
+    resolve(newarr.length);
+  });
+};
 
 module.exports = {
   promiseOutput,
